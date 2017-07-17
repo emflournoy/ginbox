e92d9a95fab8620551ad1e868a8cc90bbadbe0d3
Elizabeth Flournoy <bflournoy@gmail.com>
2017-07-17 14:58:11 -0600

    read/unread star/unstar working on singlemsg component. singlemsg is in state.

--- client/src/components/SingleMsg.js
+++ client/src/components/SingleMsg.js
@@ -10,21 +10,30 @@ export default class SingleMsg extends React.Component {
       labels: this.props.msg.labels}
   }
 
+  handleClick = (event,e)=>{
+    event.stopPropagation();
+    this.setState(prevState =>
+      ({[`${e}`]:!prevState[`${e}`]})
+    );
+  }
+
   render(props){
-    // console.log(this.state.msg);
     return(
-      <div className={`row message ${this.state.read ? 'read':'unread'}`}>
+      <div className={`row message ${this.state.read ? 'read':'unread'}`} onClick={(event)=>this.handleClick(event,'read')}>
         <div className="col-xs-1">
           <div className="row">
             <div className="col-xs-2">
               <input type="checkbox" />
             </div>
             <div className="col-xs-2">
-              <i className="star fa fa-star-o"></i>
+              <i className={`star fa ${this.state.starred ? 'fa-star':'fa-star-o'}`} onClick={(event)=>this.handleClick(event,'starred')}></i>
             </div>
           </div>
         </div>
         <div className="col-xs-11">
+          {this.state.labels.map((label, i) => {
+              return <span key={i} className="label label-warning">{label}</span>
+          })}
           <a href="#">
             {this.state.subject}
           </a>
